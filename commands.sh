fed40bc9efc38347f5a662203f613a56414c6e9b
root <jaimevalero78@yahoo.es>
2015-02-20 11:58:26 +0100

    Feature: Tenant daily usage

--- commands.sh
+++ commands.sh
@@ -1,1 +1,25 @@
 nova usage-list <intervalo_date>
+cinder list
+cinder quota-usage <tenant_name>
+cinder service-list
+keystone ec2-credentials-list
+keystone endpoint-list
+keystone role-list
+keystone service-list 
+keystone tenant-list
+keystone user-list 
+keystone user-list --tenant <tenant_name>
+neutron quota-list
+nova aggregate-list
+nova flavor-list
+nova floating-ip-pool-list
+nova host-list
+nova hypervisor-stats
+nova hypervisor-list
+nova hypervisor-servers <hypervisor>
+nova hypervisor-show <hypervisor>
+nova image-list
+nova list --all-tenants
+nova net-list
+nova quota-defaults
+nova usage-list <intervalo_date>
